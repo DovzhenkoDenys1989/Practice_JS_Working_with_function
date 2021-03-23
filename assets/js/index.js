ec46284bc56aa67853d0bd5e956fc2b6c5818d09
DovzhenkoDenys1989 <ddv10jan1989@gmail.com>
2021-03-23 23:10:48 +0200

    Task 2: minValue

--- assets/js/index.js
+++ assets/js/index.js
@@ -7,12 +7,30 @@ const userInput2 = prompt("Enter number 2: ");
 const maxValue = function(val1, val2){
  // if (val1 > val2) {
  //   return val1;
-  //  }else{
-//return val2;  }
+ //  }else{
+ //return val2;  }
    return val1 > val2 ? val1 : val2;
   }
   const maxSum = maxValue(userInput1, userInput2);
   console.log(maxSum);
 
-  
+
+
+//2. Функция, которая вернёт минимальное значение из 2.
+//Значения брать у пользователя.
+
+const minUserInput1 = prompt("Enter number 1: ");
+const minUserInput2 = prompt("Enter number 2: ");
+
+const minValue = function(minVal1, minVal2){
+//   if (minVal1 < minVal2) {
+//     return minVal1;
+//     }else{
+//   return minVal2;  }
+    return minVal1 < minVal2 ? minVal1 : minVal2;
+   }
+   const minSum = minValue(minUserInput1, minUserInput2);
+   console.log(minSum);
+ 
+
 
